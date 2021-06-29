071021e54a10669e5c9691c52e90f1fb7762f0d8
95aakash <aakash95aug@gmail.com>
2021-06-29 15:14:29 +0530

    Update index.js

--- heroku/index.js
+++ heroku/index.js
@@ -17,13 +17,12 @@ app.listen(app.get('port'));
 app.use(xhub({ algorithm: 'sha1', secret: process.env.APP_SECRET }));
 app.use(bodyParser.json());
 
-var token = 'token';
+var token = process.env.TOKEN || 'token';
 var received_updates = [];
 
 app.get('/', function(req, res) {
   console.log(req);
   res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
-//   res.send(461842958)
 });
 
 app.get(['/facebook', '/instagram'], function(req, res) {
@@ -52,12 +51,12 @@ app.post('/facebook', function(req, res) {
   res.sendStatus(200);
 });
 
-// app.post('/instagram', function(req, res) {
-//   console.log('Instagram request body:');
-//   console.log(req.body);
-//   // Process the Instagram updates here
-//   received_updates.unshift(req.body);
-//   res.sendStatus(200);
-// });
+app.post('/instagram', function(req, res) {
+  console.log('Instagram request body:');
+  console.log(req.body);
+  // Process the Instagram updates here
+  received_updates.unshift(req.body);
+  res.sendStatus(200);
+});
 
 app.listen();
